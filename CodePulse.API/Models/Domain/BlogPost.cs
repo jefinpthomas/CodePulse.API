800acd0dc335fa78cbe47c5057c02d86cbcee381
JefinPThomas <jeffinthomas646@gmail.com>
2024-11-23 22:17:49 +0530

    CodePulse

--- CodePulse.API/Models/Domain/BlogPost.cs
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -3,10 +3,12 @@
     public class BlogPost
     {
         public Guid Id { get; set; }
+
         public string Title { get; set; }
         public string ShortDescription { get; set; }
         public string content { get; set; }
         public string FeaturedImageURL { get; set; }
+        public int MyProperty { get; set; }
         public string UrlHandle { get; set; }
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
